9638deb7f0394ebb2bb52ae5f6e0cb4a12abae3c
greenshady <greenshady@dba0f204-706d-4bc1-bc29-8b92e0485636>
2010-10-20 09:16:56 +0000

    Final updates for v0.1?  Let's hope so.
    git-svn-id: http://svn.locallylost.com/themes/prototype/trunk@440 dba0f204-706d-4bc1-bc29-8b92e0485636

--- archive.php
+++ archive.php
@@ -38,6 +38,7 @@ get_header(); // Loads the header.php template. ?>
 
 						<div class="entry-summary">
 							<?php the_excerpt(); ?>
+							<?php wp_link_pages( array( 'before' => '<p class="page-links">' . __( 'Pages:', hybrid_get_textdomain() ), 'after' => '</p>' ) ); ?>
 						</div><!-- .entry-summary -->
 
 						<?php echo apply_atomic_shortcode( 'entry_meta', '<div class="entry-meta">' . __( '[entry-terms taxonomy="category" before="Posted in "] [entry-terms before="| Tagged "] [entry-comments-link before=" | "]', hybrid_get_textdomain() ) . '</div>' ); ?>
